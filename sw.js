0f8ed7436f672fc44e626cc32b311b827ec488fc
vladEgo <fordreamstudio@gmail.com>
2022-07-07 00:05:33 +0300

    sw fronm sw

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
 'use strict';
-importScripts('sw-toolbox.js'); 
-toolbox.precache(["index.html","style/style.css"]); 
-toolbox.router.get('/images/*', toolbox.cacheFirst); 
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+// importScripts('sw-toolbox.js'); 
+// toolbox.precache(["index.html","style/style.css"]); 
+// toolbox.router.get('/images/*', toolbox.cacheFirst); 
+// toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+    console.log("Service worker installed from sw");
+ });
+ self.addEventListener("activate", event => {
+    console.log("Service worker activated from sw");
+ });
